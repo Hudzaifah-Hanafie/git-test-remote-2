cce94bf4f3156738efc5a855f47e66cc236e8b4e
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2022-09-07 09:10:58 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat odio minima doloribus minus, tenetur, fuga eos praesentium, officia molestiae vitae voluptatum at cumque aperiam porro harum voluptatibus quia omnis pariatur.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
